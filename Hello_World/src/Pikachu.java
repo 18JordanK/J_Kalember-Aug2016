4839f2fe2e195cc8f1d15cb9694b17624f5b2bb1
18JordanK <18KalemberJ41@millshighsch.org>
2016-08-29 09:41:35 -0700

    My first commit

---
+++ Hello_World/src/Pikachu.java
@@ -0,0 +1,8 @@
+// Pikachu. Jordan Kalember August 29, 2016.
+public class Pikachu {
+	public static void main(String[] args){
+		System.out.println("\t\t\t\t  (\\__/)");
+		System.out.println("\t\t\t\t  (o^-^)");
+		System.out.print("\t\t\t\t z(_(\")(\")");
+	}
+}
