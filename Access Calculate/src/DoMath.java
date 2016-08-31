10e6d21470f4b2f141173d32d8638437e981a61f
18JordanK <18KalemberJ41@millshighsch.org>
2016-08-31 10:44:41 -0700

    August 31,2016 Begin Calculate Methods

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,7 @@
+
+public class DoMath {
+	public static void main(String[] args){
+		System.out.println(Calculate.average(2.0,3.0));
+		System.out.println(Calculate.discriminant(2,6,24));
+	}
+}
---
+++ Hello_World/src/TempConvert.java
@@ -0,0 +1,11 @@
+/*Jordan Kalember August 31,2016.
+ *This code converts fahrenheit into celsius.
+ */
+
+public class TempConvert {
+	public static void main(String[] args){
+		double fahrenheit = -32;
+		double celsius = (fahrenheit-32)*(5/9);
+		System.out.print(celsius);
+	}
+}
