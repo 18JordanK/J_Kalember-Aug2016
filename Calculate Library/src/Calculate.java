bcb37183ef4a9b22a9514a5384ec51bc387478a5
18JordanK <18KalemberJ41@millshighsch.org>
2016-09-01 09:48:39 -0700

    Completed Part 1 9/1/2016

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,5 +1,6 @@
 
 public class Calculate {
+	//Part 1:Methods, Headers, and Basic Math
 	public static int square(int number){
 		return(number*number);
 	}
@@ -16,15 +17,34 @@ public class Calculate {
 		return((number*number2*number3)/3);
 	}
 	
-	public static double toDegrees(double angleMeasure){
-		return(angleMeasure);
+	public static double toDegrees(double radians){
+		return(radians*3.14159/180);
 	}
 	
 	public static double toRadians(double angleMeasure){
-		return(angleMeasure);
+		return(angleMeasure*180/3.14159);
 	}
 	
-	public static double discriminant(double aValue, double bValue, double cValue){
-		return(-bValue/2/aValue);
+	public static double discriminant(double a, double b, double c){
+		return(b*b-(4*a*c));
 	}
+	
+	public static void toImproperFrac(int wholeNum, int numerator, int denominator){
+		int improperNumerator = (wholeNum*denominator) + numerator;
+		System.out.println(improperNumerator + "/" + denominator);
+	}
+	
+	public static void toMixedNum(int numerator, int denominator){
+		int wholeNum = numerator/denominator ;
+		int mixedNumerator = numerator%denominator ;
+		int mixedDenominator = denominator;
+		System.out.println(wholeNum + "_" + mixedNumerator + "/" + mixedDenominator);
+	}
+	public static void foil(int first, int inner, int outer, int last ,char var){
+		int a = first*outer;
+		int b = (first*last) - (inner*outer);
+		int c = inner*last;
+		System.out.println(a + var + "^2 +" + b + var + c);
+	}
+	// Part 2: Methods with Conditionals
 }
