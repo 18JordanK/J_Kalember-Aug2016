8b51fc344918680c7a03c10f3de31d83f21cbbbb
18JordanK <18KalemberJ41@millshighsch.org>
2017-05-05 09:27:06 -0700

    Hangman Project 5/5/17

---
+++ Hangman/src/Hangman.java
@@ -0,0 +1,16 @@
+//import javafx.application.Application;
+import java.util.*;
+
+public class Hangman {
+	public static void main(String[] Args){
+		String word = "GOODMORNING_AMERICA";
+		String completedWord = "";
+		String guess;
+		Scanner console = new Scanner(System.in);
+		
+		while(!(word.equals(completedWord))){
+			guess = console.next(".");
+			
+		}
+	}
+}
